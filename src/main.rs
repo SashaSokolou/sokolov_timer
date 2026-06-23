b0ea2bd0ba6a0d98e2626a3ba2f3dd95260b49d0
Sokolov <aleksandr.sokolov@siemens-healthineers.com>
2026-06-23 17:18:50 +0300

    after cargo +nightly fmt and cargo clippy --all-targets --all-features -- -D warnings

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,57 @@
 use rand::distr::{Alphanumeric, SampleString};
-use std::{thread, time::Duration, sync::mpsc};
-use serde::{Serialize, Deserialize};
 use rusqlite::{Connection, Result, params};
+use serde::{Deserialize, Serialize};
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::{sync::mpsc, thread, time::Duration};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-    struct CustomMessage { 
-        timestamp: i64,
-        content: String,
-        timestamp_sent: i64
-    }
+struct CustomMessage {
+    timestamp: i64,
+    content: String,
+    timestamp_sent: i64,
+}
 
-fn main() { 
-
+fn main() {
     let conn = connect_db().unwrap();
-    create_table(&conn);
+    let _ = create_table(&conn);
 
     let (tx, rx) = mpsc::channel::<String>();
 
+    let thread2 = thread::spawn(move || {
+        let mut threads_message = CustomMessage {
+            timestamp: 0,
+            content: String::new(),
+            timestamp_sent: 0,
+        };
 
-
-    let thread2 =  thread::spawn(move || {  
-
-        let mut threads_message = CustomMessage{
-        timestamp: 0,
-        content: String::new(),
-        timestamp_sent: 0
-    };        
-
-        for i in 1..31{
-            let random_string = gen_random_string();
-            threads_message.content = random_string.clone();
+        for _i in 1..31 {
+            threads_message.content = gen_random_string();
             threads_message.timestamp_sent = get_current_timestamp();
-            let serialized_message = serde_json::to_string(&threads_message).unwrap(); 
-            tx.send(serialized_message).unwrap();          
+            let serialized_message = serde_json::to_string(&threads_message).unwrap();
+            tx.send(serialized_message).unwrap();
 
-            thread::sleep(Duration::from_secs(60));            
+            thread::sleep(Duration::from_secs(1));
         }
     });
-    
-    let thread1 = thread::spawn(move || {  
-        for i in 1..31{
-            let received_message = rx.recv().unwrap(); 
-            let mut deserialized_message: CustomMessage = serde_json::from_str(&received_message).unwrap();
-            deserialized_message.timestamp = get_current_timestamp(); 
-            insert_message(&conn, deserialized_message);
-            //thread::sleep(Duration::from_secs(2)); 
+
+    let thread1 = thread::spawn(move || {
+        for _i in 1..31 {
+            let received_message = rx.recv().unwrap();
+            let mut deserialized_message: CustomMessage =
+                serde_json::from_str(&received_message).unwrap();
+            deserialized_message.timestamp = get_current_timestamp();
+            let _ = insert_message(&conn, deserialized_message);
+            //thread::sleep(Duration::from_secs(2));
         }
     });
-    thread1.join().unwrap();  
+    thread1.join().unwrap();
     thread2.join().unwrap();
-     
 }
 
 fn connect_db() -> Result<Connection> {
     let path = "./timer_db.db";
     let db: Connection = Connection::open(path)?;
-    
+
     Ok(db)
 }
 
@@ -73,7 +68,8 @@ fn create_table(conn: &Connection) -> Result<()> {
 }
 
 fn gen_random_string() -> String {
-    return Alphanumeric.sample_string(&mut rand::rng(), 255); 
+    let str = Alphanumeric.sample_string(&mut rand::rng(), 255);
+    str.to_string()
 }
 
 fn get_current_timestamp() -> i64 {
@@ -85,7 +81,7 @@ fn get_current_timestamp() -> i64 {
 fn insert_message(conn: &Connection, message: CustomMessage) -> Result<()> {
     conn.execute(
         "INSERT INTO timer (timestamp, char_string, timestamp_sent) VALUES (?1, ?2, ?3)",
-        params! [message.timestamp, message.content, message.timestamp_sent],
+        params![message.timestamp, message.content, message.timestamp_sent],
     )?;
     Ok(())
 }
